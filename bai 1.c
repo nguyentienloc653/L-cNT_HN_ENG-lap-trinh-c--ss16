505e2b3d58a1b43b747dff71cb6ca98c9f03dab9
nguyentienloc653 <locnguyentien653@gmail.com>
2024-12-16 12:11:21 +0700

    upload ss16-1

---
+++ bai 1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	int a=100;
+	int *ptra=&a;
+	printf("Cach 1:\n");
+	printf("a la %d\n", *ptra);
+	printf("Dia chi a la %d\n", ptra);
+	printf("Cach 2:\n");
+	printf("a la %d\n", a);
+	printf("Dia chi a la %d\n", &a);
+}
